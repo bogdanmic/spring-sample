8e87ec39aadfa527a0b8f6974d3775a85bd6483f
Bogdan Mic <bogdan@21stclub.com>
2017-01-17 16:46:42 +0200

    Create a simple bean and get it from the application context.

--- src/main/java/com/gd/SampleApplication.java
+++ src/main/java/com/gd/SampleApplication.java
@@ -1,12 +1,20 @@
 package com.gd;
 
+import com.gd.components.HelloBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(SampleApplication.class, args);
+
+        // Get a bean from the application context.
+        HelloBean bean = ctx.getBean(HelloBean.class);
+        System.out.println(bean.sayHello());
+
+    }
 }
