463384ef7b31c8d1569e998d946cd44171f3a996
Bogdan Mic <bogdan@21stclub.com>
2017-01-18 15:00:24 +0200

    Added slf4j logging dependency and made a few log messages.

--- src/main/java/com/gd/SampleApplication.java
+++ src/main/java/com/gd/SampleApplication.java
@@ -1,20 +1,30 @@
 package com.gd;
 
 import com.gd.components.HelloBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SampleApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(SampleApplication.class);
+
     public static void main(String[] args) {
+        logger.debug("-->START Application!!!");
         ApplicationContext ctx = SpringApplication.run(SampleApplication.class, args);
 
+        logger.info("-->Looking for Spring Bean '{}' in application context.", HelloBean.class);
         // Get a bean from the application context.
         HelloBean bean = ctx.getBean(HelloBean.class);
-        System.out.println(bean.sayHello());
+        logger.info("-->Bean [{}] found.", HelloBean.class.getName());
 
+        logger.warn("-->Using Bean {} to sayHello()", bean.getClass().getName());
+        System.out.println(bean.sayHello());
+        logger.error("-->Bean sayHello() returned:{}", bean.sayHello());
+
+        logger.debug("-->END Application!!!");
     }
 }
