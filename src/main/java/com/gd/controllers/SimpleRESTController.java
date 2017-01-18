9b6377b6d43777244717cd6f4ac5e172a9b5404b
Bogdan Mic <bogdan@21stclub.com>
2017-01-18 15:25:26 +0200

    Created simple rest controller with query variable.

--- src/main/java/com/gd/controllers/SimpleRESTController.java
+++ src/main/java/com/gd/controllers/SimpleRESTController.java
@@ -14,6 +14,13 @@ public class SimpleRESTController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @RequestMapping("/greeting")
+    public GreetingDTO greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new GreetingDTO(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+
     @RequestMapping(value = "/hello/{name}", method = RequestMethod.GET)
     public @ResponseBody GreetingDTO sayHello(@PathVariable("name") String name) {
         return new GreetingDTO(counter.incrementAndGet(), String.format(template, name));
