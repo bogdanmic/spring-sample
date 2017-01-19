9ed8e17f2d713927f67942705e0f86039d3bc4a3
Bogdan Mic <bogdan@21stclub.com>
2017-01-19 19:13:20 +0200

    Added the AOP around pointcut aspect with arguments and return value caught.

--- src/main/java/com/gd/aspects/SampleAspect.java
+++ src/main/java/com/gd/aspects/SampleAspect.java
@@ -2,10 +2,8 @@ package com.gd.aspects;
 
 import com.gd.components.HelloBean;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -25,14 +23,24 @@ public class SampleAspect {
     @Before("helloBean()")
     public void doHelloBeanAspect(JoinPoint joinPoint) {
         HelloBean target = (HelloBean) joinPoint.getTarget();
-        logger.info("-->AOP: Asspect for {} was called. The targe it: {}.", joinPoint.toString(),target);
+        logger.info("-->AOP: Asspect for {} was called. The targe it: {}.", joinPoint.toString(), target);
     }
 
     @Pointcut("target(com.gd.repositories.GreetingRepository)")
-    public void allClassesThatImplementGreetingRepository(){}
+    public void allClassesThatImplementGreetingRepository() {
+    }
 
     @After("allClassesThatImplementGreetingRepository()")
     public void doInterfaceAspect(JoinPoint joinPoint) {
         logger.info("-->AOP: Asspect for {} was called. The targe it: {}.", joinPoint.toString());
+    }
+
+    @Around(value = "execution(com.gd.dto.GreetingDTO com.gd.services.GreetingService.getGreeting(..)) && args(name)")
+    public Object doAroundWithArgument(ProceedingJoinPoint proceedingJoinPoint, String name) throws Throwable {
+        logger.warn("--> AOP Around START: For: {} with argument name={}.", proceedingJoinPoint.getTarget(), name);
+        Object returnVal = proceedingJoinPoint.proceed();
+        logger.warn("--> AOP Around END: For: {} with argument name={}. Result={}.", proceedingJoinPoint.getTarget(), name, returnVal);
+        return returnVal;
+
     }
 }
