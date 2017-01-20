9ac8dec2ca92748f34ec912b8dae5df2792c2813
Bogdan Mic <bogdan@21stclub.com>
2017-01-20 16:36:25 +0200

    With a simple annotation we can expose entities to the outside world through rest services. For some reason Customer data was exposed through rest services too, not only the profile data.

---
+++ src/main/java/com/gd/models/entities/Profile.java
@@ -0,0 +1,46 @@
+package com.gd.models.entities;
+
+import javax.persistence.*;
+
+/**
+ * Entity bean with JPA annotations Hibernate provides JPA implementation
+ */
+@Entity
+@Table(name = "profile")
+public class Profile {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
