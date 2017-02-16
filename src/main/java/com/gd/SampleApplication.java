0a2dd007d0bdc37069b991fa3e0394cc5eb4977f
Bogdan Mic <bogdan@21stclub.com>
2017-02-16 17:49:35 +0200

    Enable the application to register to consul.

--- src/main/java/com/gd/SampleApplication.java
+++ src/main/java/com/gd/SampleApplication.java
@@ -8,9 +8,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableCaching
 public class SampleApplication {
 
