cf6f2d404ffb26b97e245f199dbd43988bb81695
Bogdan Mic <bogdan@21stclub.com>
2017-02-20 21:25:50 +0200

    Configured a basic jwt auth.

--- jwt/src/main/java/com/gd/spring/JwtApplication.java
+++ jwt/src/main/java/com/gd/spring/JwtApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JwtApplication {
 
+	/**
+	 * Inspired by: https://auth0.com/blog/securing-spring-boot-with-jwts/
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(JwtApplication.class, args);
 	}
