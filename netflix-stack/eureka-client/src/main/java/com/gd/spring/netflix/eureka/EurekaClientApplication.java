22422dc5a9f72cc811d8d328583d247f363914fa
Bogdan Mic <bogdan@21stclub.com>
2017-02-22 18:04:58 +0200

    Initialize the eureka client. A service that will register itself to the eureka server

---
+++ netflix-stack/eureka-client/src/main/java/com/gd/spring/netflix/eureka/EurekaClientApplication.java
@@ -0,0 +1,12 @@
+package com.gd.spring.netflix.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+}
