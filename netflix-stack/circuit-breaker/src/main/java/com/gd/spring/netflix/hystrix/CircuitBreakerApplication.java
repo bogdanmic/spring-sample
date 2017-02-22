2ef4e9cf2cfdbd7c713ebf746a5a5fe7cd221802
Bogdan Mic <bogdan@21stclub.com>
2017-02-22 18:43:59 +0200

    Initialize the hystrix circuit breaker app and register it into eureka.

---
+++ netflix-stack/circuit-breaker/src/main/java/com/gd/spring/netflix/hystrix/CircuitBreakerApplication.java
@@ -0,0 +1,14 @@
+package com.gd.spring.netflix.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CircuitBreakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerApplication.class, args);
+	}
+}
