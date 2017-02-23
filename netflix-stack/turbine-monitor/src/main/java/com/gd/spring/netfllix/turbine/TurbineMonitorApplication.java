45d6f8195f4982a31218c8661e95f7727eced279
Bogdan Mic <bogdan@21stclub.com>
2017-02-23 18:44:08 +0200

    Initialize the turbine monitor sample app.

---
+++ netflix-stack/turbine-monitor/src/main/java/com/gd/spring/netfllix/turbine/TurbineMonitorApplication.java
@@ -0,0 +1,12 @@
+package com.gd.spring.netfllix.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurbineMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineMonitorApplication.class, args);
+	}
+}
