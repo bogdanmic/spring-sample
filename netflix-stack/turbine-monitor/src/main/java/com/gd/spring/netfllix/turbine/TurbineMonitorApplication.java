c82d0c9bcd84359ad03896b20937c88cc7c69b74
Bogdan Mic <bogdan@21stclub.com>
2017-02-23 18:49:10 +0200

    Register the turbine monitor app into eureka.

--- netflix-stack/turbine-monitor/src/main/java/com/gd/spring/netfllix/turbine/TurbineMonitorApplication.java
+++ netflix-stack/turbine-monitor/src/main/java/com/gd/spring/netfllix/turbine/TurbineMonitorApplication.java
@@ -2,8 +2,10 @@ package com.gd.spring.netfllix.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TurbineMonitorApplication {
 
 	public static void main(String[] args) {
