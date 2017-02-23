15eba27cfeea3195f5105d2a59876d233c85eacd
Bogdan Mic <bogdan@21stclub.com>
2017-02-23 10:06:14 +0200

    Enable feign

--- netflix-stack/eureka-client/src/main/java/com/gd/spring/netflix/eureka/EurekaClientApplication.java
+++ netflix-stack/eureka-client/src/main/java/com/gd/spring/netflix/eureka/EurekaClientApplication.java
@@ -3,12 +3,19 @@ package com.gd.spring.netflix.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class EurekaClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaClientApplication.class, args);
-	}
+    /**
+     * See more at: https://blog.de-swaef.eu/the-netflix-stack-using-spring-boot-part-3-feign/
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
 }
