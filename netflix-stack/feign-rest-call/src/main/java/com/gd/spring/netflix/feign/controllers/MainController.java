e292ab51e53dae7f89ccc3048ea0f4f83b027f3e
Bogdan Mic <bogdan@21stclub.com>
2017-02-23 11:40:39 +0200

    Use the new feign client inside our service.

--- netflix-stack/feign-rest-call/src/main/java/com/gd/spring/netflix/feign/controllers/MainController.java
+++ netflix-stack/feign-rest-call/src/main/java/com/gd/spring/netflix/feign/controllers/MainController.java
@@ -1,8 +1,8 @@
 package com.gd.spring.netflix.feign.controllers;
 
+import com.gd.spring.netflix.feign.dtos.InstanceDTO;
 import com.gd.spring.netflix.feign.services.SearchInstancesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +16,7 @@ public class MainController {
     private SearchInstancesService searchInstancesService;
 
     @RequestMapping("/search-service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
+    public List<InstanceDTO> serviceInstancesByApplicationName(
             @PathVariable String applicationName) {
         return searchInstancesService.searchServiceInstancesByApplicationName(applicationName);
     }
--- netflix-stack/feign-rest-call/src/main/java/com/gd/spring/netflix/feign/services/SearchInstancesService.java
+++ netflix-stack/feign-rest-call/src/main/java/com/gd/spring/netflix/feign/services/SearchInstancesService.java
@@ -1,14 +1,19 @@
 package com.gd.spring.netflix.feign.services;
 
-import org.springframework.cloud.client.ServiceInstance;
+import com.gd.spring.netflix.feign.dtos.InstanceDTO;
+import com.gd.spring.netflix.feign.rest.clients.SearchServicesResource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
 public class SearchInstancesService {
-    public List<ServiceInstance> searchServiceInstancesByApplicationName(String applicationName) {
-        return Collections.emptyList();
+
+    @Autowired
+    private SearchServicesResource searchServicesResource;
+
+    public List<InstanceDTO> searchServiceInstancesByApplicationName(String applicationName) {
+        return searchServicesResource.serviceInstancesByApplicationName(applicationName);
     }
 }
