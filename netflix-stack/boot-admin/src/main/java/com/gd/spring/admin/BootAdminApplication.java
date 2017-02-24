7621f0872f7a7804b3a1d1e6904a0db7c4abcb6e
Bogdan Mic <bogdan@21stclub.com>
2017-02-24 10:52:32 +0200

    Added the spring boot admin server dependency and enabled it.

--- netflix-stack/boot-admin/src/main/java/com/gd/spring/admin/BootAdminApplication.java
+++ netflix-stack/boot-admin/src/main/java/com/gd/spring/admin/BootAdminApplication.java
@@ -1,11 +1,13 @@
 package com.gd.spring.admin;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAdminServer
 public class BootAdminApplication {
 
 	public static void main(String[] args) {
