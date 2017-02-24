66bfaa4246d40702c80864d3643768a02e1ae60d
Bogdan Mic <bogdan@21stclub.com>
2017-02-24 10:24:22 +0200

    Initialize the Spring boot admin module

---
+++ netflix-stack/boot-admin/src/main/java/com/gd/spring/admin/BootAdminApplication.java
@@ -0,0 +1,14 @@
+package com.gd.spring.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAdminApplication.class, args);
+	}
+}
