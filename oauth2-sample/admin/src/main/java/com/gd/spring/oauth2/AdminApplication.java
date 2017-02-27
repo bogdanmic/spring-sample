df992d06e4fd0c71f84edcef5f85f1f1c79d3d23
Bogdan Mic <bogdan@21stclub.com>
2017-02-27 23:33:23 +0200

    Initialize the admin app.

---
+++ oauth2-sample/admin/src/main/java/com/gd/spring/oauth2/AdminApplication.java
@@ -0,0 +1,12 @@
+package com.gd.spring.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+}
