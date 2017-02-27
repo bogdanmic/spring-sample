2dfd234ceb13e9fe0036f28b6678a8b3940bc597
Bogdan Mic <bogdan@21stclub.com>
2017-02-27 23:20:24 +0200

    Initialize the gateway app

---
+++ oauth2-sample/gateway/src/main/java/com/gd/spring/oauth2/GatewayApplication.java
@@ -0,0 +1,12 @@
+package com.gd.spring.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
