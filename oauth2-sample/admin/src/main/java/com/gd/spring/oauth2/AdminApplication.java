b603d8365bc98993cea7606e66a53cb20b4e05cf
Bogdan Mic <bogdan@21stclub.com>
2017-02-27 23:39:23 +0200

    Added url to the tutorial.

--- oauth2-sample/admin/src/main/java/com/gd/spring/oauth2/AdminApplication.java
+++ oauth2-sample/admin/src/main/java/com/gd/spring/oauth2/AdminApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdminApplication {
 
+	/**
+	 * For more: https://spring.io/guides/tutorials/spring-security-and-angular-js/
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(AdminApplication.class, args);
 	}
