cbb327828ee723d065c754d5ce90c62eec4e60d3
Bogdan Mic <bogdan@21stclub.com>
2017-02-27 17:37:37 +0200

    Fetch a resource from the server and display it.

---
+++ oauth2-sample/single-page-app/src/main/java/com/gd/spring/oauth2/controllers/MainController.java
@@ -0,0 +1,20 @@
+package com.gd.spring.oauth2.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/resource")
+    public Map<String, Object> home() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("id", UUID.randomUUID().toString());
+        model.put("content", "Hello World");
+        return model;
+    }
+}
--- oauth2-sample/single-page-app/src/main/resources/static/js/main.js
+++ oauth2-sample/single-page-app/src/main/resources/static/js/main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('hello', [])
-    .controller('home', function () {
-        this.greeting = {id: 'xxx', content: 'Hello World!'}
+    .controller('home', function ($http) {
+        var self = this;
+        $http.get('/resource/').then(function (response) {
+            self.greeting = response.data;
+        });
     });
