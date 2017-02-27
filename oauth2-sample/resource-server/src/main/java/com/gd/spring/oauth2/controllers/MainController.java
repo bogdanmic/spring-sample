bff48e3b586320d631deb9320c4901c78a627911
Bogdan Mic <bogdan@21stclub.com>
2017-02-27 21:49:22 +0200

    Remove some of spring security setup.

--- oauth2-sample/resource-server/src/main/java/com/gd/spring/oauth2/controllers/MainController.java
+++ oauth2-sample/resource-server/src/main/java/com/gd/spring/oauth2/controllers/MainController.java
@@ -1,7 +1,6 @@
 package com.gd.spring.oauth2.controllers;
 
 import com.gd.spring.oauth2.dto.MessageDTO;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     @RequestMapping("/")
-    @CrossOrigin(origins = "*", maxAge = 3600,
-            allowedHeaders={"x-auth-token", "x-requested-with"})
     public MessageDTO home() {
         // In case the @CrossOrigin annotation doesn't work, take a look at: http://stackoverflow.com/questions/32319396/cors-with-spring-boot-and-angularjs-not-working
         return new MessageDTO("Hello World");
