5613a05b1198062c460893f9728a3ee8fdd7218b
Bogdan Mic <bogdan@21stclub.com>
2017-02-27 21:20:16 +0200

    Add the oauth2 server dependency and enable it.

--- oauth2-sample/auth-server/src/main/java/com/gd/spring/oauth2/AuthServerApplication.java
+++ oauth2-sample/auth-server/src/main/java/com/gd/spring/oauth2/AuthServerApplication.java
@@ -2,8 +2,10 @@ package com.gd.spring.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class AuthServerApplication {
 
 	public static void main(String[] args) {
