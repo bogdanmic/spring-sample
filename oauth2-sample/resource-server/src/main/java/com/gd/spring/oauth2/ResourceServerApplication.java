f258bef4afd0e776fa1296173b0652d00124d5e0
Bogdan Mic <bogdan@21stclub.com>
2017-02-27 18:40:41 +0200

    Allow cors

--- oauth2-sample/resource-server/src/main/java/com/gd/spring/oauth2/ResourceServerApplication.java
+++ oauth2-sample/resource-server/src/main/java/com/gd/spring/oauth2/ResourceServerApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ResourceServerApplication {
 
+	/**
+	 * For more info: https://spring.io/guides/tutorials/spring-security-and-angular-js/
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ResourceServerApplication.class, args);
 	}
--- oauth2-sample/resource-server/src/main/java/com/gd/spring/oauth2/controllers/MainController.java
+++ oauth2-sample/resource-server/src/main/java/com/gd/spring/oauth2/controllers/MainController.java
@@ -1,6 +1,7 @@
 package com.gd.spring.oauth2.controllers;
 
 import com.gd.spring.oauth2.dto.MessageDTO;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     @RequestMapping("/resource")
+    @CrossOrigin(origins="*", maxAge=3600)
     public MessageDTO home() {
         return new MessageDTO("Hello World");
     }
