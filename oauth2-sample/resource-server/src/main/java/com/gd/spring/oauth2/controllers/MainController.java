045985946fa86c8f3e6085ac622ac58f5487cabe
Bogdan Mic <bogdan@21stclub.com>
2017-02-28 00:44:16 +0200

    Added some aditional endpoints that will be used by admin or by user roles.

--- oauth2-sample/resource-server/src/main/java/com/gd/spring/oauth2/controllers/MainController.java
+++ oauth2-sample/resource-server/src/main/java/com/gd/spring/oauth2/controllers/MainController.java
@@ -1,16 +1,42 @@
 package com.gd.spring.oauth2.controllers;
 
+import com.gd.spring.oauth2.dto.ChangeDTO;
 import com.gd.spring.oauth2.dto.MessageDTO;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
 public class MainController {
 
-    @RequestMapping("/")
+    private String message = "Hello World";
+    private List<ChangeDTO> changes = new ArrayList<>();
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public MessageDTO home() {
-        // In case the @CrossOrigin annotation doesn't work, take a look at: http://stackoverflow.com/questions/32319396/cors-with-spring-boot-and-angularjs-not-working
-        return new MessageDTO("Hello World");
+        return new MessageDTO(message);
+    }
+
+    @RequestMapping(value = "/changes", method = RequestMethod.GET)
+    public List<ChangeDTO> changes() {
+        return changes;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public MessageDTO update(@RequestBody MessageDTO map, Principal principal) {
+        if (map.getContent() != null) {
+            message = map.getContent();
+            changes.add(new ChangeDTO(principal.getName(), message));
+            while (changes.size() > 10) {
+                changes.remove(0);
+            }
+        }
+        return new MessageDTO(message);
     }
 }
