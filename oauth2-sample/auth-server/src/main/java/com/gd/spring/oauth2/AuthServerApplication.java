9ac2aeb6ff2f1075e6e0e4727e72e3b497e52d4a
Bogdan Mic <bogdan@21stclub.com>
2017-02-27 20:49:26 +0200

    Initialize the auth server module.

---
+++ oauth2-sample/auth-server/src/main/java/com/gd/spring/oauth2/AuthServerApplication.java
@@ -0,0 +1,12 @@
+package com.gd.spring.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApplication.class, args);
+	}
+}
