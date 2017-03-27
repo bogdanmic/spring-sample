0fe3c68285936326c0f59d0b48eb7bb0a7bbe9f1
Bogdan Mic <bogdan@21stclub.com>
2017-03-27 18:25:52 +0300

    Annotate class with constructor autowire.

--- playground/src/main/java/com/gd/spring/services/GreetingServiceImpl.java
+++ playground/src/main/java/com/gd/spring/services/GreetingServiceImpl.java
@@ -2,6 +2,7 @@ package com.gd.spring.services;
 
 import com.gd.spring.dto.GreetingDTO;
 import com.gd.spring.repositories.GreetingRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,18 +10,13 @@ import java.util.concurrent.atomic.AtomicLong;
 
 
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class GreetingServiceImpl implements GreetingService {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    private GreetingRepository greetingRepository;
-
-    @Autowired
-    public void setGreetingRepository(GreetingRepository greetingRepository) {
-        // Here we have a setter Spring Bean Injection
-        this.greetingRepository = greetingRepository;
-    }
+    private final GreetingRepository greetingRepository;
 
     @Override
     public GreetingDTO getGreeting(String name) {
