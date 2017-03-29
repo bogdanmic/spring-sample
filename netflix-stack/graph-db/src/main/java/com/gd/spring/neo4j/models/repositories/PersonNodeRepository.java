d5a66f646595ddc3372de96c7e8d05ef42083afc
Bogdan Mic <bogdan@21stclub.com>
2017-03-29 14:23:01 +0300

    Add some comments.

--- netflix-stack/graph-db/src/main/java/com/gd/spring/neo4j/models/repositories/PersonNodeRepository.java
+++ netflix-stack/graph-db/src/main/java/com/gd/spring/neo4j/models/repositories/PersonNodeRepository.java
@@ -7,5 +7,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PersonNodeRepository extends GraphRepository<PersonNode> {
 
+    /**
+     * More info at: http://docs.spring.io/spring-data/data-neo4j/docs/4.2.1.RELEASE/reference/html/
+     * @param name
+     * @return
+     */
     PersonNode findByName(String name);
 }
