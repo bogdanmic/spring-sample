64c535e2578a7128dae1e26e50583dd62b8677fb
Bogdan Mic <bogdan@21stclub.com>
2017-03-29 16:28:38 +0300

    Create two entities to index/insert into elasticsearch.

--- netflix-stack/elastic-search/src/main/java/com/gd/spring/elasticsearch/ElasticSearchApplication.java
+++ netflix-stack/elastic-search/src/main/java/com/gd/spring/elasticsearch/ElasticSearchApplication.java
@@ -2,8 +2,10 @@ package com.gd.spring.elasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ElasticSearchApplication {
 
 	public static void main(String[] args) {
